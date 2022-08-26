d12decf9494db7515746825364a74a7089009062
Ryan Kelln <ryan.kelln@gmail.com>
2022-08-26 15:47:09 -0400

    beat matching

--- images.gd
+++ images.gd
@@ -5,11 +5,16 @@ extends Node2D
 @export_node_path(Label) var _actualFrameNode
 @export_node_path(Label) var _runningTotalFramesNode
 
-var transition := true
 var gui := true
 
+# transitions
+var transition := true
+var transition_tween : Tween
+var transition_cutoff := 0.042 # 24 fps = 0.042
 var _prevTexture : Texture2D
-var _anim_fps : float  # stores animation fps (only done when animation starts)
+var _next_transition_delay : SceneTreeTimer
+
+
 
 func _init_node(nodeOrPath, defaultPath) -> Node:
 	if nodeOrPath is Node:
@@ -21,17 +26,13 @@ func _init_node(nodeOrPath, defaultPath) -> Node:
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_frameNode = _init_node(_frameNode, "Frame")
-	_totalFramesNode = _init_node(_totalFramesNode, "TotalFrames")
-	_actualFrameNode = _init_node(_actualFrameNode, "ActualFrame")
-	_runningTotalFramesNode = _init_node(_runningTotalFramesNode, "RunningTotal")
+	if gui:
+		_frameNode = _init_node(_frameNode, "Frame")
+		_totalFramesNode = _init_node(_totalFramesNode, "TotalFrames")
+		_actualFrameNode = _init_node(_actualFrameNode, "ActualFrame")
+		_runningTotalFramesNode = _init_node(_runningTotalFramesNode, "RunningTotal")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
+	
 func _on_animated_sprite_2d_frame_changed():
 	# update GUI
 	if gui:
@@ -56,18 +57,19 @@ func _on_animated_sprite_2d_frame_changed():
 		_prevTexture = $AnimatedSprite2D.get_current_frame()
 		if $PrevImage.texture != null:
 			var duration = $AnimatedSprite2D.get_frame_duration()
+			#printt("frame change", $AnimatedSprite2D.frame, duration)
 			# HACK: when duration is low then start more transparent
 			var from = clampf(duration * 2.0, 0.5, 1.0) 
-			if duration > 0.042: # 24 fps
+			if duration > transition_cutoff:
 				$PrevImage.visible = true
 				# copy scale and offset
 				# (luckily this works because this signal seems to be processed before the AnimatedSPite is updated, phew)
 				if $AnimatedSprite2D.stretch:
 					$PrevImage.scale = $AnimatedSprite2D.scale
 					$PrevImage.offset = $AnimatedSprite2D.offset
-				var tween = get_tree().create_tween()
-				tween.tween_property($PrevImage, "modulate:a", 0.0, duration).from(from)
-				# TODO: if differnet size then fade in the new image for half the duration?
+				transition_tween = get_tree().create_tween()
+				transition_tween.tween_property($PrevImage, "modulate:a", 0.0, duration).from(from)
+				# TODO: if different size then fade in the new image for half the duration?
 			else:
 				$PrevImage.visible = false
 
@@ -89,20 +91,39 @@ func load_sequence(file_path : String):
 		$AnimatedSprite2D.change_animation(sequence_name) # switch to loaded sequence
 
 
-func get_total_frame_count():
+func get_total_frame_count() -> int:
+	if $AnimatedSprite2D.frames == null: return 0
+	
 	var count := 0
 	for anim in $AnimatedSprite2D.frames.get_animation_names():
 		count += $AnimatedSprite2D.frames.get_frame_count(anim)
 	return count
 
 
-func get_frame_count(anim_name : String) -> int:
+func get_frame_count(anim_name : String = "") -> int:
+	if $AnimatedSprite2D.frames == null: return 0
+	
+	if anim_name == "":
+		anim_name = $AnimatedSprite2D.animation
+		
 	return $AnimatedSprite2D.frames.get_frame_count(anim_name)
 
 
-func get_frames() -> SpriteFrames:
+func get_spriteframes() -> SpriteFrames:
 	return $AnimatedSprite2D.frames
-	
+
+
+func get_frames(anim_name : String = "") -> Array:
+	if anim_name == "":
+		anim_name = $AnimatedSprite2D.animation
+		
+	var sp = get_spriteframes()
+	var count = get_frame_count()
+	var frames = Array()
+	for i in range(min(count, 100)):
+		frames.append(sp.get_frame(anim_name, i))
+	return frames
+
 
 func get_save_path() -> String:
 	return $AnimatedSprite2D.save_path
@@ -110,3 +131,38 @@ func get_save_path() -> String:
 
 func save_frames(file_path : String) -> int:
 	return $AnimatedSprite2D.save_frames(file_path)
+
+
+func pause():
+	$AnimatedSprite2D.pause()
+	if  transition_tween.is_valid() and transition_tween.is_running():
+		transition_tween.pause()
+	
+	
+func resume():
+	$AnimatedSprite2D.resume()
+	if  transition_tween.is_valid():
+		transition_tween.play()
+
+
+func set_timing(duration_ms : float):
+	var dur_s = duration_ms / 1000.0
+	$AnimatedSprite2D.set_frame_duration(dur_s)
+	if  transition_tween.is_valid() and transition_tween.is_running():
+		transition_tween.stop()
+	$PrevImage.visible = false
+	#$AnimatedSprite2D.next_frame() # immediately advance to sync to beat
+	
+	# try to adjust for transition
+	if dur_s > transition_cutoff:
+		if _next_transition_delay and _next_transition_delay.time_left > 0:
+			printt("delay transition", dur_s / 2.0)
+			_next_transition_delay.time_left = dur_s / 2.0
+		else:
+			_next_transition_delay = get_tree().create_timer(dur_s / 2.0)
+			printt("start delay", dur_s / 2.0)
+			await _next_transition_delay.timeout
+			printt("offset transition")
+			$AnimatedSprite2D.next_frame()
+	else:
+		$AnimatedSprite2D.next_frame() # immediately advance to sync to beat
