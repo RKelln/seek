4287b27c4e156ef3a761ac433231a521c5f01f02
Ryan Kelln <ryan.kelln@gmail.com>
2022-10-11 11:13:06 -0400

    clean up code

--- HelpPopup.gd
+++ HelpPopup.gd
@@ -3,7 +3,6 @@ extends Window
 
 func _input(event : InputEvent) -> void:
 	if visible and event is InputEventKey and not event.pressed:
-		printt("help", event)
 		if event.is_action_released("ui_cancel") or event.is_action_released('help'):
 			close()
 
