f0e976dd7a4bc357ccd08a94d9b07da86c2a54fe
Ryan Kelln <ryan.kelln@gmail.com>
2023-11-24 15:57:19 -0500

    mid and settings for concert 2

--- midi_controller.gd
+++ midi_controller.gd
@@ -10,10 +10,10 @@ const knob_top_1 := { 'channel': 0, 'message': 11, 'controller': 48 }
 const knob_top_2 := { 'channel': 0, 'message': 11, 'controller': 49 }
 const knob_top_3 := { 'channel': 0, 'message': 11, 'controller': 50 }
 const knob_top_4 := { 'channel': 0, 'message': 11, 'controller': 51 }
-#const knob_top_5 := { 'channel': 0, 'message': 11, 'controller': 52 }
-#const knob_top_6 := { 'channel': 0, 'message': 11, 'controller': 53 }
-#const knob_top_7 := { 'channel': 0, 'message': 11, 'controller': 54 }
-#const knob_top_8 := { 'channel': 0, 'message': 11, 'controller': 55 }
+const knob_top_5 := { 'channel': 0, 'message': 11, 'controller': 52 }
+const knob_top_6 := { 'channel': 0, 'message': 11, 'controller': 53 }
+const knob_top_7 := { 'channel': 0, 'message': 11, 'controller': 54 }
+const knob_top_8 := { 'channel': 0, 'message': 11, 'controller': 55 }
 
 # knobs on middle right
 const knob_right_1 := { 'channel': 0, 'message': 11, 'controller': 16 }
@@ -29,11 +29,11 @@ const knob_right_8 := { 'channel': 0, 'message': 11, 'controller': 23 }
 const fader_1 := { 'channel': 0, 'message': 11, 'controller': 7 }
 const fader_2 := { 'channel': 1, 'message': 11, 'controller': 7 }
 const fader_3 := { 'channel': 2, 'message': 11, 'controller': 7 }
-#const fader_4 := { 'channel': 3, 'message': 11, 'controller': 7 }
-#const fader_5 := { 'channel': 4, 'message': 11, 'controller': 7 }
-#const fader_6 := { 'channel': 5, 'message': 11, 'controller': 7 }
-#const fader_7 := { 'channel': 6, 'message': 11, 'controller': 7 }
-#const fader_8 := { 'channel': 7, 'message': 11, 'controller': 7 }
+const fader_4 := { 'channel': 3, 'message': 11, 'controller': 7 }
+const fader_5 := { 'channel': 4, 'message': 11, 'controller': 7 }
+const fader_6 := { 'channel': 5, 'message': 11, 'controller': 7 }
+const fader_7 := { 'channel': 6, 'message': 11, 'controller': 7 }
+const fader_8 := { 'channel': 7, 'message': 11, 'controller': 7 }
 const fader_master := { 'channel': 0, 'message': 11, 'controller': 14 }
 
 # clip stop buttons
@@ -84,18 +84,18 @@ func _handle_input(event : InputEvent) -> void:
 		ev.strength = event.controller_value / midi_max_value
 		
 		match m:
-			knob_top_1:
-				ev.action = &"set_speed"
-				ev.target = 2
-			knob_top_2:
-				ev.action = &"set_transition_duration"
-				ev.target = 2
-			knob_top_3:
-				ev.action = &"set_opacity"
-				ev.target = 2
-			knob_top_4:
-				ev.action = &"set_manual_transition"
-				ev.target = 2
+			#knob_top_1:
+				#ev.action = &"set_speed"
+				#ev.target = 2
+			#knob_top_2:
+				#ev.action = &"set_transition_duration"
+				#ev.target = 2
+			#knob_top_3:
+				#ev.action = &"set_opacity"
+				#ev.target = 2
+			#knob_top_4:
+				#ev.action = &"set_manual_transition"
+				#ev.target = 2
 			knob_right_1:
 				ev.action = &"set_speed"
 				ev.target = 0
@@ -120,18 +120,18 @@ func _handle_input(event : InputEvent) -> void:
 			knob_right_8:
 				ev.action = &"set_manual_transition"
 				ev.target = 1
-			fader_1:
-				ev.action = &"set_opacity"
-				ev.target = 0
-			fader_2:
-				ev.action = &"set_opacity"
-				ev.target = 1
-			fader_3:
-				ev.action = &"set_opacity"
-				ev.target = 2
-			fader_master:
-				ev.action = &"set_manual_transition"
-				ev.target = 0
+			#fader_1:
+				#ev.action = &"set_opacity"
+				#ev.target = 0
+			#fader_2:
+				#ev.action = &"set_opacity"
+				#ev.target = 1
+			#fader_3:
+				#ev.action = &"set_opacity"
+				#ev.target = 2
+			#fader_master:
+				#ev.action = &"set_manual_transition"
+				#ev.target = 0
 
 	# note events
 	if event.message == message_note_on or event.message == message_note_off:
